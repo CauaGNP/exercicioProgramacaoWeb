2d0f2b6d92d930caaaefd393b1ca2b4e81ebe6ff
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2024-09-20 23:10:16 +0000

    preprarando Ex8 e 9

---
+++ ex8/script.js
@@ -0,0 +1,21 @@
+const idInput = document.querySelector('#idInput');
+const buttonId = document.querySelector('#buttonId');
+const textPre = document.querySelector('#textPre');
+
+buttonId.onclick = buttonIdFetchClick;
+
+async function fetcha(){
+    const inputValue = String(idInput.value)
+    const fetchURL = `https://swapi.dev/api/people/${inputValue}`
+    const fetch_ = await fetch(fetchURL)
+    .then((resposta) => {
+        console.log(resposta)
+    });
+
+    const awaitJSON = await fetch_.json()
+    console.log(awaitJSON)
+}
+
+function buttonIdFetchClick(){
+    fetcha()
+}

10acb4340938b1297e2a393aada2eaa3614bf3fb
CauaGNP <cauagnp13@gmail.com>
2024-09-23 22:14:34 -0300

    mudando de http para https

--- ex8/pag2.js
+++ ex8/pag2.js
@@ -5,7 +5,7 @@ const textoBuscaDados = document.querySelector('#textoBuscaDados');
 const buttonDadosFetchClick = () => {
     const buscaDadosValue = inputBuscaDados.value.trim();
 
-    fetch(`http://api.weatherstack.com/current?access_key=003983a79486b39217ab23785884b952&query=${buscaDadosValue}`)
+    fetch(`https://api.weatherstack.com/current?access_key=003983a79486b39217ab23785884b952&query=${buscaDadosValue}`)
     .then(response => response.json())
     .then((data) => {
         textoBuscaDados.innerText = JSON.stringify(data)
